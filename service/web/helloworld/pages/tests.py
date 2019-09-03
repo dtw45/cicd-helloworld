30e5a0e4148e9aca12ecebfdd30215661165bdae
dtw45 <derekwladyka@gmail.com>
2019-09-03 08:27:32 -0400

    Add test cases

--- service/web/helloworld/pages/tests.py
+++ service/web/helloworld/pages/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+import random
 
-# Create your tests here.
+from django.test import Client, TestCase
+from django.urls import reverse
+
+class ViewTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_home(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_random(self):
+        result = random.choice((True, False))
+        self.assertEqual(result, True)
+
