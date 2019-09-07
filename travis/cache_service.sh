704fee36b7cd581c055dc90a6044a07d856f4288
dtw45 <derekwladyka@gmail.com>
2019-09-07 07:51:47 -0400

    cat to echo

--- travis/cache_service.sh
+++ travis/cache_service.sh
@@ -8,4 +8,5 @@ IMAGES=$(docker images -a \
     --filter='dangling=false' \
     --format '{{.Repository}}:{{.Tag}} {{.ID}}')
 
-cat "$IMAGES" | xargs -n 2 -t sh -c 'test -e $HOME/docker/$1.tar.gz || docker save $0 | gzip -2 > $HOME/docker/$1.tar.gz'
+echo "$IMAGES" | xargs -n 2 -t sh -c 'test -e $HOME/docker/$1.tar.gz || docker save $0 | gzip -2 > $HOME/docker/$1.tar.gz'
+
